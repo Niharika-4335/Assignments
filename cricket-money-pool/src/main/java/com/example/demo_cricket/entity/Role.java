23bb9b5a68b6061804e62943922cd5fb3a7d0ae6
Niharika-4335 <niharikabethapudi71@gmail.com>
2025-05-21 22:46:30 +0530

    Delete cricket-money-pool directory

--- cricket-money-pool/src/main/java/com/example/demo_cricket/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo_cricket.entity;
-
-public enum Role {
-    PLAYER,
-    ADMIN
-}
-//if we create enum we don't need to hardcode values  of roles.we just need one table for both admin and user.
--- cricket-money-pool/src/main/java/com/example/demo_cricket/repository/WalletRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo_cricket.repository;
-
-import com.example.demo_cricket.entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet,Integer> {
-
-}
